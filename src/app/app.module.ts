130ee836557bda5626cbfbf20bc454369d23eae3
sherin200 <sherinsebastian21>
2022-12-11 00:03:42 +0530

    user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,32 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { UserLoginComponent } from './user-login/user-login.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { UserRegisterComponent } from './user-register/user-register.component';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
+const myRoutes:Routes = [
+  {
+    path:"",
+    component:AdminLoginComponent
+  },
+  {
+    path:"userlogin",
+    component:UserLoginComponent
+  },
+  {
+    path:"userReg",
+    component:UserRegisterComponent
+  },
+  {
+    path:"dashboard",
+    component:DashBoardComponent
+  },
+  {
+    path:"userProfile",
+    component:UserProfileComponent
+  }
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -24,7 +49,10 @@ import { UserRegisterComponent } from './user-register/user-register.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(myRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
